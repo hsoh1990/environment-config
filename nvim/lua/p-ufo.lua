89419911d16f06f2f1b1c577bf48ddfc19e7f3b7
wellstone <ohs4123@gmail.com>
2023-03-30 20:13:40 +0900

    neo vim config change

--- nvim/lua/p-ufo.lua
+++ nvim/lua/p-ufo.lua
@@ -1,5 +1,3 @@
-use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
-
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
@@ -9,13 +7,8 @@ vim.o.foldenable = true
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
--- Option 1: coc.nvim as LSP client
 require('ufo').setup()
---
 
--- Option 2: nvim lsp as LSP client
--- Tell the server the capability of foldingRange,
--- Neovim hasn't added foldingRange to default capabilities, users must add it manually
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
   dynamicRegistration = false,
@@ -25,26 +18,5 @@ local language_servers = require("lspconfig").util.available_servers() -- or lis
 for _, ls in ipairs(language_servers) do
   require('lspconfig')[ls].setup({
     capabilities = capabilities
-    -- you can add other fields for setting up lsp server in this table
   })
 end
-require('ufo').setup()
---
-
--- Option 3: treesitter as a main provider instead
--- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
--- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
-require('ufo').setup({
-  provider_selector = function(bufnr, filetype, buftype)
-    return { 'treesitter', 'indent' }
-  end
-})
---
-
--- Option 4: disable all providers for all buffers
--- Not recommend, AFAIK, the ufo's providers are the best performance in Neovim
-require('ufo').setup({
-  provider_selector = function(bufnr, filetype, buftype)
-    return ''
-  end
-})
