3030f26557993307bb4ad26f48f649362509d91b
hsoh1990 <ohs4123@gamil.com>
2023-04-07 14:39:36 +0900

    fix. nvim config

--- nvim/lua/p-ufo.lua
+++ nvim/lua/p-ufo.lua
@@ -3,12 +3,35 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
--- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
+local ftMap = {
+  vim = 'indent',
+  python = { 'indent' },
+  git = ''
+}
 require('ufo').setup({
-  provider_selector = function()
-    return { 'treesitter', 'indent' }
+  open_fold_hl_timeout = 150,
+  close_fold_kinds = { 'imports', 'comment' },
+  preview = {
+    win_config = {
+      border = { '', '─', '', '', '', '─', '', '' },
+      winhighlight = 'Normal:Folded',
+      winblend = 0
+    },
+    mappings = {
+      scrollU = '<C-u>',
+      scrollD = '<C-d>',
+      jumpTop = '[',
+      jumpBot = ']'
+    }
+  },
+  provider_selector = function(_, filetype, _)
+    -- if you prefer treesitter provider rather than lsp,
+    return ftMap[filetype] or { 'treesitter', 'indent' }
+    -- return ftMap[filetype]
   end
 })
+
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
